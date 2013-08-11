91815f2d0a20441cda9e67eb1ed93dad451bb7d8
0x6C77 <0x6C77@gmail.com>
2013-08-11 18:38:25 +0100

    Fixing __autoload

--- phpunit/appTest.php
+++ phpunit/appTest.php
@@ -1,13 +1,12 @@
 <?php
     function __autoload($class) {
-        echo realpath($_SERVER["DOCUMENT_ROOT"] . '/../');
-
         require_once realpath($_SERVER["DOCUMENT_ROOT"] . '/../') . '/class.'.$class.'.php';
     }
 
     class APPTest extends PHPUnit_Framework_TestCase {
         public function __construct() {
             session_start();
+            spl_autoload_register('__autoload');
         }
 
         public function testAppInit() {
